bf023b200ec282d7f40cfffbf53e2e09f1831b54
bulutluoz <abbulutluoz@gmail.com>
2023-09-17 10:43:46 +0200

    3.ders sonu

---
+++ src/test/java/tests/day03_JUnit/C05_TestNotasyonu.java
@@ -0,0 +1,21 @@
+package tests.day03_JUnit;
+
+import org.junit.Test;
+
+public class C05_TestNotasyonu {
+
+
+    @Test
+    public void test01(){
+        System.out.println("Test01 calisti");
+    }
+
+    @Test
+    public void test02(){
+        System.out.println("Test02 calisti");
+    }
+    @Test
+    public void test03(){
+        System.out.println("Test03 calisti");
+    }
+}
