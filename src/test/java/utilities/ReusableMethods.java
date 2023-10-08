f3e96c0aecba2573d11376d5a88df8995994407d
bulutluoz <abbulutluoz@gmail.com>
2023-10-08 08:37:41 +0200

    9.gun sonu

--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.WebDriver;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class ReusableMethods {
 
@@ -20,10 +22,19 @@ public class ReusableMethods {
     }
 
 
-    public static void tumSayfaFotografCek(WebDriver driver)  {
+    public static void tumSayfaFotografCek(WebDriver driver,String screenshotIsmi)  {
 
         TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
-        File tumSayfaSS = new File("target/screenshots/tumSayfaSS.png");
+
+        // dosya adini dinamik yapalim
+        // target/screenshots/tumSayfaSS.png
+        LocalDateTime localDateTime = LocalDateTime.now();
+        DateTimeFormatter istenenFormat = DateTimeFormatter.ofPattern("yyMMddHHmm");
+        localDateTime.format(istenenFormat); // 2310080829
+
+        String dinamikDosyaAdi = "target/screenshots/"+screenshotIsmi+
+                                    localDateTime.format(istenenFormat)+".jpg";
+        File tumSayfaSS = new File(dinamikDosyaAdi);
         File geciciDosya = takesScreenshot.getScreenshotAs(OutputType.FILE);
 
         try {
@@ -31,5 +42,19 @@ public class ReusableMethods {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+        /*
+            screenshot dosyasi icin hep ayni ismi kullanirsak
+            tum cekilen fotograflar ayni isimle kaydedileceginden
+            tek bir dosya olur, son cekilen fotograf eskilerin uzerine kaydolur
+
+            cektigimiz fotograflarin hepsinin kalmasi
+            ve istenilen isimde olmasi icin
+            dosya adini dinamik yapalim
+
+            1- method'un cagrildigi yerde bir fotograf ismi yazilsin
+            2- method'da dosya ismine tarih etiketi ekleyelim
+            ilkUrun2310080829
+         */
     }
 }
