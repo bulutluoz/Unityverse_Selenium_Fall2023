3af36d997738aa908e324b1ce3a9882b6ca32dd3
bulutluoz <abbulutluoz@gmail.com>
2023-10-08 08:19:32 +0200

    9.gun sonu

---
+++ src/test/java/tests/day09_screenshot_JsExecutor_TestNG/C01_TumSayfaScreenshot.java
@@ -0,0 +1,39 @@
+package tests.day09_screenshot_JsExecutor_TestNG;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import utilities.ReusableMethods;
+import utilities.TestBase;
+
+public class C01_TumSayfaScreenshot extends TestBase {
+
+    @Test
+    public void test01(){
+
+        // amazon anasayfaya gidin
+        driver.get("https://www.amazon.com");
+        // url'in amazon icerdigini test edin ve screenshot alin
+        String expectedUrlIcerik = "amazon";
+        String actualUrl = driver.getCurrentUrl();
+        Assert.assertTrue(actualUrl.contains(expectedUrlIcerik));
+        ReusableMethods.tumSayfaFotografCek(driver);
+        // Nutella icin arama yapin
+        WebElement aramaKutusu = driver.findElement(By.id("twotabsearchtextbox"));
+        aramaKutusu.sendKeys("Nutella" + Keys.ENTER);
+        // ilk urune tiklayin
+        driver.findElement(By.xpath("(//div[@class='a-section aok-relative s-image-square-aspect'])[1]"))
+                .click();
+        // urun isminin Nutella icerdigini test edin ve screenshot alin
+
+        WebElement urunIsimElementi= driver.findElement(By.xpath("//h1[@id='title']"));
+
+        String expectedUrunIcerik = "Nutella";
+        String actualUrunIsmi = urunIsimElementi.getText();
+
+        Assert.assertTrue(actualUrunIsmi.contains(expectedUrunIcerik));
+        ReusableMethods.tumSayfaFotografCek(driver);
+    }
+}
--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -1,5 +1,13 @@
 package utilities;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+import java.io.IOException;
+
 public class ReusableMethods {
 
     public static void bekle(int saniye){
@@ -7,6 +15,20 @@ public class ReusableMethods {
         try {
             Thread.sleep(saniye*1000);
         } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public static void tumSayfaFotografCek(WebDriver driver)  {
+
+        TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+        File tumSayfaSS = new File("target/screenshots/tumSayfaSS.png");
+        File geciciDosya = takesScreenshot.getScreenshotAs(OutputType.FILE);
+
+        try {
+            FileUtils.copyFile(geciciDosya,tumSayfaSS);
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
